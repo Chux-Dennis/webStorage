d6b8841f50e8dcbc095951368a027874935d4b60
Chux-Dennis <134112520+Chux-Dennis@users.noreply.github.com>
2023-09-28 20:39:19 +0100

    Debug Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const themeIcon = document.querySelector(".fa-sun");
 
-themeIcon.addEventListener("click", () => {
+function changeTheme() {
   // changing the body apperance from the button click
   document.querySelector("html").classList.toggle("light");
   const theme = document.documentElement.classList.length - 1;
@@ -9,6 +9,18 @@ themeIcon.addEventListener("click", () => {
   localStorage.setItem("theme", document.documentElement.classList[theme]);
 
   themeIcon.classList.toggle("fa-moon");
+}
+
+// Initiating a change after button click
+themeIcon.addEventListener("click", () => changeTheme());
+
+// Initiating a change after keypress
+document.body.addEventListener("keydown", (e) => {
+  if (e.shiftKey) {
+    if (e.code === "KeyT") {
+      changeTheme();
+    }
+  }
 });
 
 // Checking browser storage for any saved theme data
